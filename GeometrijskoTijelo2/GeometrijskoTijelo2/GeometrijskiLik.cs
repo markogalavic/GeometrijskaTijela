c769070e5238b6c39908bb0d1b14a740811c7014
markogalavic <55134792+markogalavic@users.noreply.github.com>
2021-02-08 21:55:51 +0100

    Add files via upload

---
+++ GeometrijskoTijelo2/GeometrijskoTijelo2/GeometrijskiLik.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometrijskoTijelo2
+{
+    class GeometrijskiLik
+    {
+        int BrojStranica = 0;
+        double Stranice=[10];
+
+        public void UcitajStranice()
+        {
+            GeometrijskiLik Kocka = new Cetverokut();
+            GeometrijskiLik Trokut = new Trokut();
+        }
+    }
+}
---
+++ GeometrijskoTijelo2/GeometrijskoTijelo2/Trokut.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometrijskoTijelo2
+{
+    class Trokut : GeometrijskiLik
+    {
+        int stranice = 3;
+    }
+}
